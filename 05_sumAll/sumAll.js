f05993643f3a3f4a3357f54eeca8270dac32a257
Naramala siva venkata pavan <sivavenkatapavan@gmail.com>
2024-06-26 19:52:58 +0530

    Complete the exercises in the repository

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(lo,hi) {
+    if(lo<0||hi<0||typeof(lo)!='number'||typeof(hi)!="number"){
+        return 'ERROR';
+    }
+    if(lo>hi){
+        lo=lo^hi;
+        hi=lo^hi;
+        lo=lo^hi;
+    }
+    let sum=0;
+    for(let i=lo;i<=hi;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+//c-0/100=f-32/180
+const convertToCelsius = function(fahrenheit) {
+  return parseFloat(((fahrenheit-32)*100/180).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return parseFloat((celsius*180/100+32).toFixed(1));
 };
 
 // Do not edit below this line
